2892232e1d329626d9f0fe5a4e5f5aaa561f6c6b
NgaiYeanCoi <2232047183@stayrabbit.top>
2024-11-27 20:07:46 +0800

    Synchronization

--- src/main/java/xyz/nyc1/Demo.java
+++ src/main/java/xyz/nyc1/Demo.java
@@ -3,11 +3,11 @@ package xyz.nyc1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Demo {
     static JFrame mainFrame = new JFrame("test");
-    private  JButton receiveAcceptBtn = new JButton("接受");
-    private JButton receiveDeclineBtn = new JButton("拒绝");
+
 
     public static void DemoTest() {
 
@@ -19,7 +19,6 @@ public class Demo {
 
 
 
-        //
 
         // 创建模态对话框
         JDialog receiveFileRequestDialog = new JDialog(mainFrame, "接收文件", true); // 设置为模态
@@ -30,25 +29,37 @@ public class Demo {
         receiveFileRequestDialog.setLayout(new BorderLayout()); // 设置布局
         receiveFileRequestDialog.setBackground(new Color(255, 255, 255));
 
-        String ipPort="127.0.0.1:10086";
+        //创建主Panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(new Color(255, 255, 255));
+        mainPanel.setLayout(new BorderLayout());
+
+
+        //center面板
         JPanel centerPanel = new JPanel();
         centerPanel.setBackground(new Color(255, 255, 255));
-        centerPanel.add(receiveFileRequestDialog);
-        centerPanel.setLayout(new BorderLayout());
-        JLabel centerInfo = new JLabel();
-        centerInfo.setText(ipPort+"\n想要给你给你发送一个文件");
-        centerPanel.add(centerInfo, BorderLayout.CENTER);
+        JLabel receiveInfoLabel = new JLabel();
+        centerPanel.add(receiveInfoLabel);
+        centerPanel.add(receiveInfoLabel);
+        receiveInfoLabel.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 30));
+        String ipPort ;
+        ipPort="127.0.0.1:10086";
+        receiveInfoLabel.setText(ipPort+"想要发送给你一个文件");
+
 
 
         //按钮控件
             JButton receiveAcceptBtn = new JButton("接收");
             JButton receiveDeclineBtn = new JButton("拒绝");
+        //底部面板
             JPanel bottomPanel = new JPanel();
             bottomPanel.setLayout(new FlowLayout());
             bottomPanel.add(receiveAcceptBtn);
             bottomPanel.add(receiveDeclineBtn);
-            receiveFileRequestDialog.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
-
+        //添加进主面板
+            mainPanel.add(centerPanel, BorderLayout.CENTER);
+            mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+            receiveFileRequestDialog.add(mainPanel);
 
         // 显示主框架
         mainFrame.setVisible(true);
