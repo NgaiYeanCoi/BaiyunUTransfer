623b3fd62ccb32b6721b81255d8f1527ed45dc05
NgaiYeanCoi <2232047183@stayrabbit.top>
2024-12-01 15:52:50 +0800

    feat:UI and Settings Enhancements
    - Complete the majority of UI design, ensuring a cohesive user experience.
    - Implement a comprehensive setting panel:
     - **Theme Modification**: Allow users to change the application theme.
     - **Save Path Configuration**: Enable users to set a default save path for files.
     - **Log Clearing Functionality**: Provide an function to clear application logs.
     - **GitHub Information**: Display GitHub repository in the website.

--- src/main/java/xyz/nyc1/mainUI.java
+++ src/main/java/xyz/nyc1/mainUI.java
@@ -15,6 +15,8 @@ import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.net.URI;
+import java.awt.Desktop;
 import java.util.List;
 
 import static xyz.nyc1.NetworkIpInterface.getHostIPs;
@@ -48,10 +50,21 @@ public class mainUI {
     private JButton selectFileButton;
     private JButton sendFileButton;
     private JButton cancelSelectedButton;
+    /*发送面板*/
+    private JPanel rightSendPanel;
+    private JPanel sendTopPanel;
+    private JPanel bottomPanel;
     /*接收面板*/
     private JTextArea receiveLogTextArea;
-    private JButton receiveListenButton;
-
+    private JPanel rightReceivePanel;
+    private JPanel receiveTopPanel;
+    /*设置面板*/
+    private GridBagConstraints settingGbc;
+    private JPanel settingCenterPanel;
+    private JPanel settingPanel;
+    private JPanel settingTopPanel;
+    /*默认面板*/
+    private JPanel defaultPanel;
 
 
     public mainUI() {
@@ -64,7 +77,7 @@ public class mainUI {
         mainFrame.setSize(900, 600);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLocationRelativeTo(null);
-        mainFrame.getContentPane().setBackground(Color.WHITE);
+        mainFrame.setResizable(false);
 
         // 创建主面板
         mainPanel = new JPanel(new BorderLayout());
@@ -73,11 +86,12 @@ public class mainUI {
         leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         leftPanel.setPreferredSize(new Dimension(260, 0));
-        leftPanel.add(Box.createVerticalStrut(20));
+        leftPanel.add(Box.createVerticalStrut(50));
         leftPanel.setBackground(Color.decode(mainColor));
+
         // 创建logoLabel控件
         JLabel leftLogoLabel = new JLabel("BaiyunUTransfer");
-        leftLogoLabel.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 28));
+        leftLogoLabel.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 25));
         leftLogoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         leftPanel.add(leftLogoLabel);
         leftPanel.add(Box.createVerticalStrut(40));
@@ -104,19 +118,26 @@ public class mainUI {
         cardLayout = (CardLayout) centerPanel.getLayout();
 
         // 创建右侧边栏默认面板
-        JPanel defaultPanel = new JPanel();
-        defaultPanel.add(new JLabel("请选择模式"));
+        defaultPanel = new JPanel();
         defaultPanel.setBackground(Color.decode(viceColor));
+        defaultPanel.add(Box.createVerticalStrut(500)); //设置垂直间距
+        // 创建默认面板信息部件Label
+        JLabel defaultInfoLabel = new JLabel("请选择模式");
+        defaultInfoLabel.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 25));
+        defaultInfoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        defaultInfoLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        //添加默认面板到centerPanel
+        defaultPanel.add(defaultInfoLabel);
         centerPanel.add(defaultPanel, "default");
 
         // 创建右侧边栏发送面板
-        JPanel rightSendPanel = new JPanel();
+        rightSendPanel = new JPanel();
         rightSendPanel.setLayout(new BoxLayout(rightSendPanel, BoxLayout.Y_AXIS));
         rightSendPanel.setPreferredSize(new Dimension(640, 0)); // 设置右侧栏的宽度
         rightSendPanel.add(Box.createVerticalStrut(10)); //设置垂直间距
         rightSendPanel.setBackground(Color.decode(viceColor));
         // 创建顶部面板
-        JPanel sendTopPanel = new JPanel();
+        sendTopPanel = new JPanel();
         sendTopPanel.setLayout(new FlowLayout());
         sendTopPanel.setBackground(Color.decode(viceColor));
         // 创建IPV4控件
@@ -145,36 +166,37 @@ public class mainUI {
         sendFileButton = new JButton("确认发送");
         cancelSelectedButton = new JButton("取消选择");
         /*添加组件到顶部面板*/
-        // IPv4输入面板
-        sendTopPanel.add(ipLabel);
-        sendTopPanel.add(ipSegment1);
-        sendTopPanel.add(new JLabel("."));
-        sendTopPanel.add(ipSegment2);
-        sendTopPanel.add(new JLabel("."));
-        sendTopPanel.add(ipSegment3);
-        sendTopPanel.add(new JLabel("."));
-        sendTopPanel.add(ipSegment4);
-        // 端口输入面板
-        sendTopPanel.add(sendPortLabel);
-        sendTopPanel.add(sendPortTextField);
-        // 按钮面板
-        sendTopPanel.add(connectButton);
-        sendTopPanel.add(disconnectButton);
+            // IPv4输入面板
+            sendTopPanel.add(ipLabel);
+            sendTopPanel.add(ipSegment1);
+            sendTopPanel.add(new JLabel("."));
+            sendTopPanel.add(ipSegment2);
+            sendTopPanel.add(new JLabel("."));
+            sendTopPanel.add(ipSegment3);
+            sendTopPanel.add(new JLabel("."));
+            sendTopPanel.add(ipSegment4);
+            // 端口输入面板
+            sendTopPanel.add(sendPortLabel);
+            sendTopPanel.add(sendPortTextField);
+            // 按钮面板
+            sendTopPanel.add(connectButton);
+            sendTopPanel.add(disconnectButton);
         //设置topPanel的边框
         sendTopPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         // 添加面板到右侧面板
         rightSendPanel.add(sendTopPanel,BorderLayout.NORTH);
+        rightSendPanel.add(Box.createVerticalStrut(10)); //设置垂直间距
         // 创建日志文本区域
         sendLogTextArea = new JTextArea();
         sendLogTextArea.setEditable(false);
         JScrollPane sendCenterScrollPane = new JScrollPane(sendLogTextArea);
         sendCenterScrollPane.setBorder(null);
-        sendCenterScrollPane.setPreferredSize(new Dimension(0, 400)); // 设置滚动面板的首选大小
+        sendCenterScrollPane.setPreferredSize(new Dimension(0, 500)); // 设置滚动面板的首选大小
         // 添加面板到右侧面板
         rightSendPanel.add(sendCenterScrollPane,BorderLayout.CENTER);
-        rightSendPanel.add(Box.createVerticalStrut(10));
+        rightSendPanel.add(Box.createVerticalStrut(5));
         // 创建底部面板
-        JPanel bottomPanel = new JPanel();
+        bottomPanel = new JPanel();
         bottomPanel.setLayout(new FlowLayout());
         bottomPanel.setBackground(Color.decode(viceColor));
         bottomPanel.add(selectFileButton);
@@ -186,7 +208,7 @@ public class mainUI {
         disconnectButton.setVisible(false);
         cancelSelectedButton.setVisible(false);
         // 设置bottomPanel的边框
-        bottomPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        //bottomPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         rightSendPanel.add(bottomPanel,BorderLayout.SOUTH);//添加底部面板到右侧面板
 
         // 添加连接按钮事件监听器
@@ -208,7 +230,6 @@ public class mainUI {
                 }
                 else {
                     // TODO:添加连接服务器的代码，需要连接后端API
-
                     sendLogTextArea.append("正在连接到 " + ip + ":" + port + "中...\n");
                     sendLogTextArea.append("已成功连接！\n");
                     connectButton.setVisible(false);
@@ -269,7 +290,6 @@ public class mainUI {
                 bottomPanel.setLayout(new FlowLayout());
                 bottomPanel.add(innerSelectBtn);
                 selectFileDialog.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
-                JFileChooser fileChooser = new JFileChooser();
 
 
                 // 设置拖放目标
@@ -306,7 +326,9 @@ public class mainUI {
                 innerSelectBtn.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        JFileChooser fileChooser = new JFileChooser();
                         int result = fileChooser.showOpenDialog(selectFileDialog);
+                        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
                         if (result == JFileChooser.APPROVE_OPTION) {
                             globalFilePath = fileChooser.getSelectedFile().getAbsolutePath();
                             sendLogTextArea.append("已选择文件: " + globalFilePath + "\n");
@@ -353,60 +375,58 @@ public class mainUI {
         centerPanel.add(rightSendPanel, "send");// 将功能面板添加到卡片面板中
 
         // 创建接收面板
-        JPanel rightReceivePanel = new JPanel();
+        rightReceivePanel = new JPanel();
         rightReceivePanel.setLayout(new BoxLayout(rightReceivePanel, BoxLayout.Y_AXIS));
         rightReceivePanel.setPreferredSize(new Dimension(640, 0)); // 设置右侧栏的宽度
         rightReceivePanel.add(Box.createVerticalStrut(10)); // 设置垂直间距
         rightReceivePanel.setBackground(Color.decode(viceColor));
-
         // 创建顶部面板
-        JPanel receiveTopPanel = new JPanel();
+        receiveTopPanel = new JPanel();
         receiveTopPanel.setLayout(new FlowLayout());
         receiveTopPanel.setBackground(Color.decode(viceColor));
-
         // 创建IP地址标签
         JLabel receiveIpLabel = new JLabel("IP地址:");
         receiveIpLabel.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 15));
         receiveIpLabel.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
-
         // 创建IP地址显示区域
         JTextArea ipTextArea = new JTextArea(5, 20);
         ipTextArea.setEditable(false);
         ipTextArea.setText(getHostIPs()); // 获取并显示主机的IP地址
         ipTextArea.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-
         // 创建端口Label组件
         JLabel receivePortLabel = new JLabel("端口:");
         receivePortLabel.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 15));
         receivePortLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
-
         // 创建端口输入框
         JTextField receivePortTextField = new JTextField(5);
         applyPortPattern(receivePortTextField); // 应用端口号输入正则表达式
-
         // 创建监听按钮
-        receiveListenButton = new JButton("监听端口");
-
+        JButton receiveListenBtn = new JButton("监听端口");
+        //创建停止监听按钮
+        JButton receivePauseListenBtn = new JButton("停止监听");
         // 添加组件到顶部面板
         receiveTopPanel.add(receiveIpLabel);
         receiveTopPanel.add(new JScrollPane(ipTextArea)); // 使用滚动面板支持多行文本
         receiveTopPanel.add(receivePortLabel);
         receiveTopPanel.add(receivePortTextField);
-        receiveTopPanel.add(receiveListenButton);
-
+        receiveTopPanel.add(receiveListenBtn);
+        receiveTopPanel.add(receivePauseListenBtn);
+        //初始化按钮可见
+        receiveListenBtn.setVisible(true);
+        receivePauseListenBtn.setVisible(false);
         // 添加顶部面板到接收面板
         rightReceivePanel.add(receiveTopPanel, BorderLayout.NORTH);
-
+        rightReceivePanel.add(Box.createVerticalStrut(10)); // 设置垂直间距
         // 创建日志文本区域
-
         receiveLogTextArea = new JTextArea();
         receiveLogTextArea.setEditable(false);
         JScrollPane receiveCenterScrollPane = new JScrollPane(receiveLogTextArea);
-        receiveCenterScrollPane.setPreferredSize(new Dimension(0, 400)); // 设置滚动面板的首选大小
+        receiveCenterScrollPane.setPreferredSize(new Dimension(0, 500)); // 设置滚动面板的首选大小
         receiveCenterScrollPane.setBorder(null);
         rightReceivePanel.add(receiveCenterScrollPane);
+
         // 添加监听按钮事件监听器
-        receiveListenButton.addActionListener(new ActionListener() {
+        receiveListenBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String port = receivePortTextField.getText();
@@ -420,9 +440,22 @@ public class mainUI {
                 }
                 else {
                     // TODO:添加连接服务器的代码，需要连接后端API
-                    receiveLogTextArea.append("正在监听:"+receivePortTextField.getText()+"端口中...\n");
-                }
+                    receiveLogTextArea.append("正在监听本机"+receivePortTextField.getText()+"端口中...\n");
+                    receiveListenBtn.setVisible(false);
+                    receivePauseListenBtn.setVisible(true);
 
+                }
+            }
+        });
+
+        // 添加停止监听按钮事件监听器
+        receivePauseListenBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO:需连接后端API断开
+                receiveLogTextArea.append("已停止监听\n");
+                receivePauseListenBtn.setVisible(false);
+                receiveListenBtn.setVisible(true);
             }
         });
 
@@ -432,12 +465,132 @@ public class mainUI {
 
 
         // 创建设置面板
-        JPanel settingPanel = new JPanel();
-        settingPanel.add(new JLabel("设置功能面板"));
+        settingPanel = new JPanel();
+        settingPanel.setLayout(new BoxLayout(settingPanel, BoxLayout.Y_AXIS));
+        settingPanel.setBackground(Color.decode(viceColor));
+        settingPanel.add(Box.createVerticalStrut(50));
+
+        // 创建设置顶部Panel
+        settingTopPanel = new JPanel();
+        settingTopPanel.setLayout(new BoxLayout(settingTopPanel, BoxLayout.Y_AXIS));
+        settingTopPanel.setBackground(Color.decode(viceColor));
+
+        // 创建设置顶部信息Label
+        JLabel settingInfoLabel = new JLabel("设置");
+        settingInfoLabel.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 25));
+        settingInfoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        settingPanel.add(settingInfoLabel);//将settingInfoLabel添加进settingTopPanel里
+        settingPanel.add(settingTopPanel, BorderLayout.NORTH);
+
+
+        // 创建中间设置选项面板
+        settingCenterPanel = new JPanel();
+        settingCenterPanel.setLayout(new GridBagLayout());
+        settingCenterPanel.setBackground(Color.decode(viceColor));
+        settingCenterPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+
+
+        settingGbc = new GridBagConstraints();
+        settingGbc.fill = GridBagConstraints.HORIZONTAL;
+        settingGbc.insets = new Insets(20, 50,20 , 50);
+        settingGbc.gridx = 0;
+        settingGbc.gridy = 0;
+
+        // 添加设置选项和按钮
+        //清空日志板块
+        addSettingLabel("清空所有日志");
+        JButton settingClearLogBtn = new JButton("清空");
+        addSettingBtn(settingClearLogBtn);
+        settingClearLogBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            receiveLogTextArea.setText("");
+            sendLogTextArea.setText("");
+            new InfoDialog(mainFrame,"清空成功！");
+            }
+        });
+
+        //设置保存路径板块
+        addSettingLabel("默认保存路径");
+        JButton settingDefaultPathBtn = new JButton("设置");
+        addSettingBtn(settingDefaultPathBtn);
+        JLabel settingDefaultPathLabel = new JLabel("当前的路径为：");
+        settingDefaultPathLabel.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 16));
+        settingGbc.gridx = 0;
+        settingGbc.weightx = 1.0; // 让标签占据更多空间
+        settingCenterPanel.add(settingDefaultPathLabel, settingGbc);
+        settingGbc.gridy++;  // 移动到下一行
+        settingDefaultPathBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int result = fileChooser.showOpenDialog(mainFrame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    String selectedPath = fileChooser.getSelectedFile().getAbsolutePath();
+                    //TODO：需接后端API
+                    settingDefaultPathLabel.setText("当前路径为："+selectedPath);
+                    new InfoDialog(mainFrame,"设置默认路径成功！");
+
+                }
+
+            }
+        });
+
+
+        // 修改主题样式板块
+        addSettingLabel("修改主题颜色");
+        JComboBox<String>selectStyleBox=new JComboBox<>();
+        selectStyleBox.addItem("默认");
+        selectStyleBox.addItem("白色");//TODO：可添加其他主题颜色
+        settingGbc.gridx = 1;
+        settingGbc.weightx = 0.0; // 按钮不需要额外空间
+        settingCenterPanel.add(selectStyleBox, settingGbc);
+        settingGbc.gridy++;  // 移动到下一行
+        selectStyleBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String  selectedItem=(String)selectStyleBox.getSelectedItem();
+                if("默认".equals(selectedItem)) {
+                    mainColor = "#e7ecf3";
+                    viceColor = "#f5f7fa";
+                    changeStyle(mainColor,viceColor);
+                    System.out.println("默认");
+                }
+                else if("白色".equals(selectedItem))
+                {
+                    mainColor = "#ffffff";
+                    viceColor = "#f8f9fa";
+                    changeStyle(mainColor,viceColor);
+                    System.out.println("白色");
+                }
+            }
+        });
+
+        //Github
+        addSettingLabel("Source Code (Github)");
+        JButton settingGithubBtn = new JButton("打开");
+        addSettingBtn(settingGithubBtn);
+        settingGithubBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // 打开 GitHub 主页
+                    Desktop.getDesktop().browse(new URI("https://github.com/NgaiYeanCoi/BaiyunUTransfer"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    new ErrorDialog(mainFrame,ex.getMessage());
+                }
+            }
+        });
+
+
+        settingPanel.add(settingCenterPanel,BorderLayout.CENTER);
+        // 将功能面板添加到卡片面板中
         centerPanel.add(settingPanel, "setting");
-
+        //centerPanel添加到主面板中
         mainPanel.add(centerPanel, BorderLayout.CENTER);
-
+        //主面板添加到主视窗中
         mainFrame.add(mainPanel);
 
         // 添加按钮监听事件
@@ -467,7 +620,20 @@ public class mainUI {
 
         mainFrame.setVisible(true);
     }
+    private void addSettingLabel(String labelName){
+        JLabel label = new JLabel(labelName);
+        label.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 16));
+        settingGbc.gridx = 0;
+        settingGbc.weightx = 1.0; // 让标签占据更多空间
+        settingCenterPanel.add(label, settingGbc);
+    }
+    private  void addSettingBtn(JButton button){
+        settingGbc.gridx = 1;
+        settingGbc.weightx = 0.0; // 按钮不需要额外空间
+        settingCenterPanel.add(button, settingGbc);
+        settingGbc.gridy++;  // 移动到下一行
 
+    }
     private void applyPortPattern(JTextField textField) {
         /* *
          * 端口号输入正则表达匹配检查
@@ -540,5 +706,18 @@ public class mainUI {
 
         // 获取textField的Document并设置DocumentFilter
         ((AbstractDocument) textField.getDocument()).setDocumentFilter(filter);
+    }
+    private void changeStyle(String mainColor,String viceColor){
+        leftPanel.setBackground(Color.decode(mainColor));
+        defaultPanel.setBackground(Color.decode(viceColor));
+        rightSendPanel.setBackground(Color.decode(viceColor));
+        sendTopPanel.setBackground(Color.decode(viceColor));
+        bottomPanel.setBackground(Color.decode(viceColor));
+        rightReceivePanel.setBackground(Color.decode(viceColor));
+        receiveTopPanel.setBackground(Color.decode(viceColor));
+        settingPanel.setBackground(Color.decode(viceColor));
+        settingTopPanel.setBackground(Color.decode(viceColor));
+        settingCenterPanel.setBackground(Color.decode(viceColor));
+
     }
 }
